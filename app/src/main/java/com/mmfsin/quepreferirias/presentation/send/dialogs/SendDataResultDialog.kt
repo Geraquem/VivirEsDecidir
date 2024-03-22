d966869aa29f127db76141d87ea1b73a375331b4
gers <geraquem@gmail.com>
2024-03-22 11:56:56 +0100

    wip

--- app/src/main/java/com/mmfsin/quepreferirias/presentation/send/dialogs/SendDataResultDialog.kt
+++ app/src/main/java/com/mmfsin/quepreferirias/presentation/send/dialogs/SendDataResultDialog.kt
@@ -1,12 +1,16 @@
 package com.mmfsin.quepreferirias.presentation.send.dialogs
 
 import android.app.Dialog
+import android.graphics.drawable.Drawable
+import android.graphics.drawable.GradientDrawable
 import android.view.LayoutInflater
+import androidx.core.content.ContextCompat
 import com.mmfsin.quepreferirias.R
 import com.mmfsin.quepreferirias.base.BaseDialog
 import com.mmfsin.quepreferirias.databinding.DialogSendDataResultBinding
 import com.mmfsin.quepreferirias.presentation.send.interfaces.ISendDataResultListener
 import dagger.hilt.android.AndroidEntryPoint
+
 
 @AndroidEntryPoint
 class SendDataResultDialog(
@@ -26,15 +30,28 @@ class SendDataResultDialog(
             if (wasOk) {
                 ivImage.setImageResource(R.drawable.ic_success)
                 tvTitle.text = getString(R.string.send_dualism_result_perfect_title)
-                tvSubtitle.text = getString(R.string.send_dualism_result_perfect_subtitle)
                 btnResend.text = getString(R.string.send_dualism_result_resend)
+                changeBgColor(rlTop.background, R.color.success)
+                changeBgColor(btnResend.background, R.color.white)
+                changeBgColor(btnExit.background, R.color.white)
+
             } else {
                 ivImage.setImageResource(R.drawable.ic_error)
                 tvTitle.text = getString(R.string.send_dualism_result_sww_title)
-                tvSubtitle.text = getString(R.string.send_dualism_result_sww_subtitle)
                 btnResend.text = getString(R.string.send_dualism_result_retry)
+                changeBgColor(rlTop.background, R.color.cancel)
+                changeBgColor(btnResend.background, R.color.white)
+                changeBgColor(btnExit.background, R.color.cancel)
             }
             btnExit.text = getString(R.string.send_dualism_result_exit)
+        }
+    }
+
+    private fun changeBgColor(background: Drawable, color: Int) {
+        context?.let {
+            if (background is GradientDrawable) {
+                background.setColor(ContextCompat.getColor(it, color))
+            }
         }
     }
 
