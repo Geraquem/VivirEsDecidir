4f42db662e6050981ae2b68f413aa15e9896be58
gers <geraquem@gmail.com>
2024-10-14 18:25:24 +0200

    wip

--- app/src/main/java/com/mmfsin/quepreferirias/presentation/dashboard/comments/adapter/CommentsAdapter.kt
+++ app/src/main/java/com/mmfsin/quepreferirias/presentation/dashboard/comments/adapter/CommentsAdapter.kt
@@ -6,6 +6,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.core.content.ContextCompat.getColor
+import androidx.core.view.isVisible
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 import com.mmfsin.quepreferirias.R
